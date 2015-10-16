3a336330889b19a16d9e03edd8c699a2ef4bad9d
xrage <dkverma.vit@gmail.com>
2015-10-16 19:09:35 +0530

    file_upload fix

--- app/models/audio.rb
+++ app/models/audio.rb
@@ -1,3 +1,12 @@
 class Audio < ActiveRecord::Base
   belongs_to :trip
+  has_attached_file :audio
+  FACTORY = RGeo::Geographic.simple_mercator_factory
+  set_rgeo_factory_for_column(:coordinates, FACTORY)
+  validates_attachment :audio, :content_type => { :content_type => ["audio/mp3"] }
+  validates_attachment_presence :audio
+  
+  validates_inclusion_of :acl, :in => ["friends", "me", "public"], :allow_nil => false
+  validates_presence_of :trip_id
+  
 end
